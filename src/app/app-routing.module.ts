ed066750ad0cab2aabaff0efd46e700df74725d7
Pietro Rampazzo <pietro@rampazzo.co>
2019-05-13 17:20:30 +0200

    New routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdiniComponent } from './ordini/ordini.component';
+import { TablesComponent } from './tables/tables.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tables/:id', component: TablesComponent },
+  { path: 'ordini', component: OrdiniComponent }
 ];
 
 @NgModule({
