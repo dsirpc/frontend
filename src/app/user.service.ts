9d6edfcc87097a67733572b8d57b4e2ddccc5daf
Pietro Rampazzo <pietro@rampazzo.co>
2019-05-29 21:20:15 +0200

    Url typo

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -13,7 +13,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
   public token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkNhbWVyaWVyZSIsInJvbGUiOiJXQUlURVIiLCJpYXQiOjE1NTg4NjQzODgsImV4cCI6MTU1ODg2Nzk4OH0.dcPnylnekCWSyMHWLKGBOvo9Pl26Gz0CWyXmro7lSZ0';
-  public url = 'https://dsirpc-api.herokuapp.com/';
+  public url = 'https://dsirpc-api.herokuapp.com';
 
   login(username: string, password: string, remember: boolean): Observable<any> {
 
