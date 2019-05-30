25e0ceabe8f0a3c08a93d1892378ed6017d03084
Pietro Rampazzo <pietro@rampazzo.co>
2019-05-30 18:15:23 +0200

    Login background

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 
@@ -14,28 +14,32 @@ import { Router } from '@angular/router';
   template: `<button (click)="onClickMe()">Sign in</button>`,
 })*/
 
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
   private errmessage = undefined;
-  constructor( private us: UserService, private router: Router  ) { }
+  constructor(private us: UserService, private router: Router, private elementRef: ElementRef) { }
 
   ngOnInit() {
-    this.us.renew().subscribe( (d) => {
-      console.log('Renew succeded: ' + JSON.stringify(d) );
+    this.us.renew().subscribe((d) => {
+      console.log('Renew succeded: ' + JSON.stringify(d));
       this.router.navigate(['/dashboard']);
     }, (err) => {
-      console.log('Renew error: ' + JSON.stringify(err.error.errormessage) );
+      console.log('Renew error: ' + JSON.stringify(err.error.errormessage));
     });
   }
 
-  login( username: string, password: string, remember: boolean ) {
-    this.us.login( username, password, remember).subscribe( (d) => {
-      console.log('Login granted: ' + JSON.stringify(d) );
-      console.log('User service token: ' + this.us.get_token() );
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#f5f5f5';
+  }
+
+  login(username: string, password: string, remember: boolean) {
+    this.us.login(username, password, remember).subscribe((d) => {
+      console.log('Login granted: ' + JSON.stringify(d));
+      console.log('User service token: ' + this.us.get_token());
       this.errmessage = undefined;
       this.router.navigate(['/dashboard']);
     }, (err) => {
-      console.log('Login error: ' + JSON.stringify(err.error.errormessage) );
+      console.log('Login error: ' + JSON.stringify(err.error.errormessage));
       this.errmessage = err.error.errormessage;
     });
   }
